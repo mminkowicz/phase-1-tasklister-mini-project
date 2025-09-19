4faf67002f1b0c4746a32381e6d6809a6d253c7d
Mendel Minkowicz <mminkowicz@gmail.com>
2025-09-19 12:07:29 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+const input = document.getElementById("new-task-description")
+const taskToDo = document.getElementById("tasks")
+const form = document.getElementById("create-task-form")
+
+const createNewTask = () => {
+  form.addEventListener("submit", function(event){
+    event.preventDefault()
+    const task = input.value
+    if (task !== "") {
+      const li = document.createElement("li")
+      li.textContent = task
+
+      const removeBtn = document.createElement("button")
+      removeBtn.style.marginLeft = "5px"
+      removeBtn.textContent = "X"
+      removeBtn.addEventListener("click", function() {
+        li.remove()
+      })
+
+      li.appendChild(removeBtn)
+
+      taskToDo.appendChild(li)
+      input.value = ""
+    }
+  })
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  createNewTask()
 });
